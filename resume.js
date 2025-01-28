71c738c152a200a5df47629429cc04aae461954e
cailieghcoleta <154499055+cailieghcoleta@users.noreply.github.com>
2025-01-28 20:30:40 +0800

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,8 +1,6 @@
 function changeColor(theme) {
-    // Reset all theme classes
     document.body.classList.remove('pink2-theme', 'pink3-theme', 'pink1-theme');
     
-    // Apply the selected theme class to the body
     if (theme === 'pink2') {
         document.body.classList.add('pink2-theme');
     } else if (theme === 'pink3') {
